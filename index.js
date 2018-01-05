122c731992a62c88aa1ada2f358215a028f3ca8a
Alabe Duarte <alabeduarte@gmail.com>
2018-01-05 15:49:44 +1100

    chore(build): bootstrap application

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const MyComponent = () => <div>Hello World!</div>;
+
+ReactDOM.render(<MyComponent/>, document.querySelector('#root'));
