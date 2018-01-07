fb6dfc0115ea5bba799f245cbdfdfc347e754e45
Alabe Duarte <alabeduarte@gmail.com>
2018-01-08 09:46:44 +1100

    chore(build): add logger to the server

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const logger = require('./logger');
 const Server = require('./web');
 
 app.use('/thermometer-sensor', new Server());
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => {
+  logger.info(`Listening on port ${PORT}`);
+});
