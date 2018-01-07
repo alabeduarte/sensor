7ad5531264036a850d9a522ab58405e97fb5edc8
Alabe Duarte <alabeduarte@gmail.com>
2018-01-08 10:50:24 +1100

    refactor(sensor): extract statuses map to be outside

--- server/thermometer-sensor/out-of-range/index.js
+++ server/thermometer-sensor/out-of-range/index.js
@@ -1,12 +1,11 @@
 const isTrue = requirement => requirement === true;
+const statuses = {
+  true: 'NORMAL',
+  false: 'OUT_OF_RANGE'
+};
 
 module.exports = function OutOfRange({ pointValue, range }) {
   const { min, max } = range;
-  const statuses = {
-    true: 'NORMAL',
-    false: 'OUT_OF_RANGE'
-  };
-
   const evaluation = [pointValue >= min, pointValue <= max].every(isTrue);
 
   return {
