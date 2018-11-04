f0285d81fe438c6760b32a8635ac4a1f5064108f
Alabe Duarte <alabeduarte@gmail.com>
2018-11-04 20:16:11 +1100

    feat(web-client): list all sensor temperature changes

--- web-client/src/App/index.spec.jsx
+++ web-client/src/App/index.spec.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { random } from 'faker';
 import App from './'
+import Sensors from './Sensors';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
@@ -14,21 +15,27 @@ describe('<App />', () => {
     const sensor1 = {
       data: {
         uuid: random.uuid(),
-        currentTemperature: 4
+        currentTemperature: 4,
+        idealTemperatureRange: {
+          min: 0,
+          max: 7
+        }
       }
     };
 
     const sensor2 = {
       data: {
         uuid: random.uuid(),
-        currentTemperature: 2
+        currentTemperature: 2,
+        idealTemperatureRange: {
+          min: 0,
+          max: 7
+        }
       }
     };
 
     const wrapper = mount(<App sensors={[sensor1, sensor2]} />);
-    const items = wrapper.find('.sensors li');
 
-    expect(items.at(0).text()).toContain('Current Temperature: 4ºC');
-    expect(items.at(1).text()).toContain('Current Temperature: 2ºC');
+    expect(wrapper.find('.sensors li').length).toEqual(2);
   });
 });
